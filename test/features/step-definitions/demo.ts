6d548171d66a380651b51ace4793e8546f5b9f21
TejuTaware <tejashree.t@dntinfotech.com>
2022-06-29 18:21:08 +0530

    Add input handling

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,7 +1,7 @@
 import { Given, When, Then} from "@cucumber/cucumber";
 import {expect} from "chai"
 
-Given(/^Google page is opened$/, async function () {
+/** Given(/^Google page is opened$/, async function () {
     console.log(`Before opening browser...`);
     await browser.url("https://www.google.com")
     await browser.pause(1000)
@@ -24,4 +24,19 @@ Then(/^URL should match (.*)$/, async function (ExpectedURL) {
     console.log(`>> expectedURL: ${ExpectedURL}`);
     let url = await browser.getUrl()
     expect(url).to.equal(ExpectedURL)
+})*/
+/**
+ * Web Interactions
+ */
+Given(/^A web page is opened$/, async function(){
+    await browser.url("https://the-internet.herokuapp.com/inputs")
+    await browser.setTimeout({implicit: 15000, pageLoad: 10000})
+    await browser.maximizeWindow()
+})
+
+When(/^Perform web interactions$/, async function(){
+    let ele = await $(`[type=number]`)
+    await ele.setValue("12345")
+
+    await browser.debug()
 })
